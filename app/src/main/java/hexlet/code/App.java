ce86f63f3de930d38d8a94d8aed9d71a27024a66
Kirill Vasilev <kirillwork56@gmail.com>
2026-04-22 13:25:35 +0300

    add step 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
-public class App {
+import java.util.concurrent.Callable;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "gendiff", description = "Compares two configuration files and shows a difference.",
+        mixinStandardHelpOptions = true, version = "1.0")
+public class App implements Callable<Integer> {
+
+    @Override
+    public Integer call() {
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
