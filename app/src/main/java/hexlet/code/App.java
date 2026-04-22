425c807d5e007a6434fc291d67d917f802c67f18
Kirill Vasilev <kirillwork56@gmail.com>
2026-04-22 18:19:00 +0300

    add step 4 and 5

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,16 +1,55 @@
 package hexlet.code;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff", description = "Compares two configuration files and shows a difference.",
         mixinStandardHelpOptions = true, version = "1.0")
 public class App implements Callable<Integer> {
 
+    @Parameters(paramLabel = "filepath1", index = "0", description = "path to first file")
+    private Path filepath1;
+
+    @Parameters(paramLabel = "filepath2", index = "1", description = "path to second file")
+    private Path filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]",
+            paramLabel = "format", arity = "1")
+    private String format = "stylish";
+
+    public static Map<String, Object> getData(Path path) throws Exception {
+        String content = readFile(path);
+        return parse(content);
+    }
+
+    public static Map<String, Object> parse(String content) throws Exception {
+        return Map.of();
+    }
+
+    public static String readFile(Path path) throws Exception {
+        return Files.readString(path);
+    }
+
     @Override
     public Integer call() {
-        return 0;
+        try {
+            var data1 = getData(filepath1);
+            var data2 = getData(filepath2);
+
+            System.out.println(data1);
+            System.out.println(data2);
+
+            return 0;
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            return 1;
+        }
     }
 
     public static void main(String[] args) {
