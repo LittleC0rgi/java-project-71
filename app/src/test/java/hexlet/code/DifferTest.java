3f1758e9d4041c9d8aae1914dbec36bd1c45790b
Kirill Vasilev <kirillwork56@gmail.com>
2026-05-05 16:29:33 +0300

    fix for style check

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DifferTest {
     private static String getFilePath(String filename) {
