d3e760f043fe89e60287157483a97676df51c420
Kirill Vasilev <kirillwork56@gmail.com>
2026-05-13 19:03:36 +0300

    ref Parser

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -3,22 +3,38 @@ package hexlet.code;
 import hexlet.code.formatter.FormatType;
 import hexlet.code.formatter.Formatter;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 
 public class Differ {
     public static String generate(String filePath1, String filePath2, String format) throws Exception {
-        Map<String, Object> d1 = Parser.getData(Path.of(filePath1));
-        Map<String, Object> d2 = Parser.getData(Path.of(filePath2));
+        Path firstFile = Path.of(filePath1);
+        String firstFileContent = readFile(firstFile);
+        String firstFileFormat = getFormat(firstFile);
+
+        Path secondFile = Path.of(filePath2);
+        String secondFileContent = readFile(secondFile);
+        String secondFileFormat = getFormat(secondFile);
+
+        Map<String, Object> d1 = Parser.parse(firstFileContent, firstFileFormat);
+        Map<String, Object> d2 = Parser.parse(secondFileContent, secondFileFormat);
         var type = FormatType.from(format);
         var tree = DiffCalculator.calculateDiff(d1, d2);
         return Formatter.format(type, tree);
     }
 
     public static String generate(String filePath1, String filePath2) throws Exception {
-        Map<String, Object> d1 = Parser.getData(Path.of(filePath1));
-        Map<String, Object> d2 = Parser.getData(Path.of(filePath2));
-        var tree = DiffCalculator.calculateDiff(d1, d2);
-        return Formatter.format(FormatType.STYLISH, tree);
+        return generate(filePath1, filePath2, FormatType.STYLISH.toString());
+    }
+
+    private static String readFile(Path path) throws Exception {
+        return Files.readString(path);
+    }
+
+    private static String getFormat(Path path) {
+        String fileName = path.getFileName().toString();
+        int dotIndex = fileName.lastIndexOf(".");
+        return dotIndex == -1 ? "" : fileName.substring(dotIndex + 1);
     }
 }
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,39 +1,21 @@
 package hexlet.code;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Map;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
+import java.util.Map;
+
 public class Parser {
-
-    public static Map<String, Object> getData(Path path) throws Exception {
-        String content = readFile(path);
-        String format = getFormat(path);
-        return parse(content, format);
+    public static Map<String, Object> parse(String data, String format) throws Exception {
+        ObjectMapper mapper = getMapper(format);
+        return mapper.readValue(data, Map.class);
     }
 
-    public static String readFile(Path path) throws Exception {
-        return Files.readString(path);
-    }
-
-    public static Map<String, Object> parse(String content, String format) throws Exception {
-        ObjectMapper mapper;
-
+    private static ObjectMapper getMapper(String format) {
         if (format.equals("yaml") || format.equals("yml")) {
-            mapper = new ObjectMapper(new YAMLFactory());
-        } else {
-            mapper = new ObjectMapper();
+            return new ObjectMapper(new YAMLFactory());
         }
 
-        return mapper.readValue(content, Map.class);
-    }
-
-    private static String getFormat(Path path) {
-        String fileName = path.getFileName().toString();
-        int dotIndex = fileName.lastIndexOf(".");
-        return dotIndex == -1 ? "" : fileName.substring(dotIndex + 1);
+        return new ObjectMapper();
     }
 }
